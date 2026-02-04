9ec5b2f97e3b26fdfeb31428abdd0baa75ff3047
Soft Well House <bialonj@gmail.com>
2026-02-04 21:17:34 +0100

    fix docker & package

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
+// Base path must match vite.config.js base option
+const basename = import.meta.env.BASE_URL || '/task-list/'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
